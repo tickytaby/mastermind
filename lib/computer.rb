7c0ea9f678b9c0a633d152dca374ae73c5a59058
tickytaby <riccardo.yin@outlook.com>
2024-09-30 16:23:25 +0200

    Minor changes to make the code work

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,10 +1,10 @@
 class Computer
-  attr_accessor :colors
+  attr_accessor :colors 
   def initialize(colors = ['Red', 'Blue', 'Yellow', 'Green', 'White', 'Black'])
     @colors = colors
   end
 
   def generate_code
-    Array.new(4) {@colors.sample}
+    @secret_code = Array.new(4) {@colors.sample}
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,11 @@
 class Player
 
-  def guess(x)
+  def guess(x, secret_code)
     x.each_with_index do |color, index|
       case
-      when color == @secret_code[index]
+      when color == secret_code[index]
         puts "Color #{color} is correct and in the right position at #{index}"
-      when @secret_code.include?(color)
+      when secret_code.include?(color)
         puts "Color #{color} is correct but in the wrong position"
       else
         puts "Color #{color} is not in the secret code"
